76a7f13130c66cfa1689ea4cc04b67fcc47f6592
noah <noah@penners.net>
2018-12-05 09:58:21 -0600

    Established a connection.

---
+++ DoctorDatabase/src/doctordatabase/actions.java
@@ -0,0 +1,26 @@
+package doctordatabase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class actions {
+
+	private static Connection connect() {
+		Connection conn = null;
+		
+		try {
+			String url = "H:\\git\\DoctorDatabase\\DoctorDatabase";
+			conn = DriverManager.getConnection(url);
+			System.out.println("Connection to SQLite has been established.");
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return conn;
+	}
+	
+	
+}
