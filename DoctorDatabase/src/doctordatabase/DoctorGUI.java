b6078157edd2773aaaa1f2af88c456b8492043b4
noah <noah@penners.net>
2018-12-06 09:52:49 -0600

    GUI updates

--- DoctorDatabase/src/doctordatabase/DoctorGUI.java
+++ DoctorDatabase/src/doctordatabase/DoctorGUI.java
@@ -1,5 +1,83 @@
 package doctordatabase;
 
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
 public class DoctorGUI {
+	
+	public DoctorGUI() {
+		JFrame frame = new JFrame();
+		JTable table = new JTable();
+		frame.setSize(400, 400);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel(new GridBagLayout());
+		
+		GridBagConstraints c = new GridBagConstraints();
+		
+		Object[] rows = {"Username", "Password", "Enter"};
+		
+		JLabel user = new JLabel("Username");
+		c.gridx = 0;
+		c.gridy = 0;
+		
+		panel.add(user, c);
+		
+		
+		JLabel pass = new JLabel("Password");
+		c.gridx = 0;
+		c.gridy = 1;
+		
+		panel.add(pass, c);
+		
+		JButton enter = new JButton("Enter");
+		c.gridx = 0;
+		c.gridy = 2;
+		
+		panel.add(enter, c);
+		
+		JTextField usert = new JTextField(20);
+		c.gridx = 1;
+		c.gridy = 0;
+		
+		panel.add(usert, c);
+		
+		JTextField passt = new JTextField(20);
+		c.gridx = 1;
+		c.gridy = 1;
+		
+		panel.add(passt, c);
+		
+		//////////////////////////Button Action Listener START////////////////////////////////
+		
+		enter.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		//////////////////////////////Button Action Listener END////////////////////////////
+		
+		frame.add(panel);
+		//frame.setLayout(null);
+		frame.setVisible(true);
+		
+	}
 
 }
---
+++ DoctorDatabase/src/doctordatabase/Instance.java
@@ -0,0 +1,9 @@
+package doctordatabase;
+
+public class Instance {
+
+	public static void main(String[] args) {
+		DoctorGUI GUI = new DoctorGUI();
+	}
+
+}
