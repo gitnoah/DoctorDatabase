61e9e195baf870b1f99e837b61b05666349bebaa
noah <noah@penners.net>
2018-12-14 09:49:58 -0600

    fgd

--- DoctorDatabase/src/doctordatabase/DoctorGUI.java
+++ DoctorDatabase/src/doctordatabase/DoctorGUI.java
@@ -27,258 +27,281 @@ public class DoctorGUI {
 	DefaultTableModel model;
 	Object[] row;
 	int DoctorID;
-	
+
 	public DoctorGUI() {
 		JFrame frame = new JFrame();
 		JTable table = new JTable();
 		frame.setSize(400, 400);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panel = new JPanel(new GridBagLayout());
-		
-		GridBagConstraints c = new GridBagConstraints();
-		
-		Object[] rows = {"Username", "Password", "Enter"};
-		
 
-		
+		JPanel panel = new JPanel(new GridBagLayout());
+
+		GridBagConstraints c = new GridBagConstraints();
+
+		Object[] rows = { "Username", "Password", "Enter" };
+
 		JLabel user = new JLabel("Username");
 		c.gridx = 0;
 		c.gridy = 0;
-		
+
 		panel.add(user, c);
-		
-		
+
 		JLabel pass = new JLabel("Password");
 		c.gridx = 0;
 		c.gridy = 1;
-		
+
 		panel.add(pass, c);
-		
+
 		JButton enter = new JButton("Enter");
 		c.gridx = 0;
 		c.gridy = 2;
-		
+
 		panel.add(enter, c);
-		
+
 		JTextField usert = new JTextField(20);
 		c.gridx = 1;
 		c.gridy = 0;
-		
+
 		panel.add(usert, c);
-		
+
 		JTextField passt = new JTextField(20);
 		c.gridx = 1;
 		c.gridy = 1;
-		
+
 		panel.add(passt, c);
-		
+
 		JLabel passwordcheck = new JLabel("Wrong Password.");
 		c.gridx = 1;
 		c.gridy = 2;
-		
+
 		panel.add(passwordcheck, c);
 		passwordcheck.setVisible(false);
-		
-	//////////////////////New Frame///////////////////////////////////
-		
+
+		////////////////////// New Frame///////////////////////////////////
+
 		JFrame frame2 = new JFrame();
 		JTable table2 = new JTable();
 		frame2.setSize(1200, 1200);
 		frame2.setLocationRelativeTo(null);
 		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel panel2 = new JPanel(new GridBagLayout());
-		
+
 		GridBagConstraints c2 = new GridBagConstraints();
-		
-		/////////////////////
-		Object[] columns = {"Name","Phone"};
+
+		///////////// Patient Table Start////////
+		Object[] columns = { "Name", "Phone" };
 		model = new DefaultTableModel();
 		model.setColumnIdentifiers(columns);
-		model.addRow(new Object[] {"Column 1", "Column 2"});
+		model.addRow(new Object[] { "Column 1", "Column 2" });
 		table2.setModel(model);
-		
+
 		table2.setRowHeight(30);
-		
+
 		JScrollPane pane1 = new JScrollPane(table2);
 		pane1.setVisible(false);
 		c2.gridx = 0;
 		c2.gridy = 5;
 		c2.gridwidth = 4;
 		pane1.setSize(600, 600);
-		
+
 		panel2.add(pane1, c2);
 		frame2.setLayout(null);
-		////////////////////
-		
-		/////////////////////add patient options////////////////////////////
-		
+		//////////// Patient Table End////////
+
+		///////////////////// add patient options start////////////////////////////
+
 		JLabel pname = new JLabel("Enter name of Patient");
-		c.gridy = 1;
-		c.gridx = 0;
-		
+		c2.gridy = 1;
+		c2.gridx = 0;
+		c2.gridwidth = 1;
+
 		pname.setVisible(false);
-		
-		panel2.add(pname, c);
-		
+
+		panel2.add(pname, c2);
+
 		JLabel pnum = new JLabel("Enter phone number");
-		c.gridy = 2;
-		c.gridx = 0;
-		
+		c2.gridy = 2;
+		c2.gridx = 0;
+		c2.gridwidth = 1;
+
 		pnum.setVisible(false);
-		
-		panel2.add(pnum, c);
-		
-		JTextField pnamt = new JTextField(30);
-		c.gridy = 1;
-		c.gridx = 1;
-		
+
+		panel2.add(pnum, c2);
+
+		JTextField pnamt = new JTextField(25);
+		c2.gridy = 1;
+		c2.gridx = 2;
+		c2.gridwidth = 2;
+
 		pnamt.setVisible(false);
-		
-		panel2.add(pnamt, c);
-		
-		JTextField pphont = new JTextField(30);
-		c.gridy = 2;
-		c.gridx = 1;
-		
+
+		panel2.add(pnamt, c2);
+
+		JTextField pphont = new JTextField(25);
+		c2.gridy = 2;
+		c2.gridx = 2;
+		c2.gridwidth = 2;
+
 		pphont.setVisible(false);
-		
-		panel2.add(pphont);
-		
+
+		panel2.add(pphont, c2);
+
 		JButton entpat = new JButton("Enter");
-		c.gridy = 3;
-		c.gridx = 0;
-		
+		c2.gridy = 3;
+		c2.gridx = 0;
+		c2.gridwidth = 1;
+
 		entpat.setVisible(false);
-		
-		panel2.add(entpat);
-		
+
+		panel2.add(entpat, c2);
+
 		entpat.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				String sql = "INSERT INTO PatientTable (Name, Phone number, DoctorID) VALUES (?, ?, ?)";
-				
+				System.out.println("Enter patient button.");
+				String sql = "INSERT INTO PatientTable (Name, Phonenumber, DoctorID) VALUES (?, ?, ?)";
+
 				try {
-					
+
 					Connection conn = connect();
 					PreparedStatement ps = conn.prepareStatement(sql);
-					
-				}catch(SQLException e) {
+
+					ps.setString(1, pnamt.getText());
+					ps.setString(2, pphont.getText());
+					ps.setInt(3, DoctorID);
+
+					ps.executeUpdate();
+					ps.close();
+					conn.close();
+
+				} catch (SQLException e) {
 					System.out.println(e.getMessage());
 				}
-				
+
+				refresh(DoctorID);
+
 			}
-			
+
 		});
-		
-		
-		
-		
-		
-		
-		//////////////////////////////
-		
+
+		///////////////////// add patient options end/////////
+
+		////////////////////// display visits start/////////////////
+
+		/////////////////////// display visits end///////////////////
+
 		JButton display = new JButton("Display Patients.");
 		c2.gridx = 0;
 		c2.gridy = 0;
 		c2.gridwidth = 1;
-		
+
 		panel2.add(display, c2);
-		
+
 		display.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println("Display Patients Button");
+				pname.setVisible(false);
+				pnum.setVisible(false);
+				pnamt.setVisible(false);
+				pphont.setVisible(false);
+				entpat.setVisible(false);
+
 				pane1.setVisible(true);
-				Object[] columns = {"Name","Phone"};
+				Object[] columns = { "Name", "Phone" };
 				model = new DefaultTableModel();
 				model.setColumnIdentifiers(columns);
 				table2.setModel(model);
 				table2.setRowHeight(30);
-				
+
 				refresh(DoctorID);
-				
+
 			}
-			
+
 		});
-		
+
 		JButton newp = new JButton("New Patients.");
 		c2.gridx = 1;
 		c2.gridy = 0;
 		c2.gridwidth = 1;
-		
+
 		panel2.add(newp, c2);
-		
+
 		newp.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println("New Patients");
 				pane1.setVisible(true);
-				Object[] columns = {"Name","Phone"};
+				Object[] columns = { "Name", "Phone" };
 				model = new DefaultTableModel();
 				model.setColumnIdentifiers(columns);
 				table2.setModel(model);
 				table2.setRowHeight(30);
-				
+
+				pname.setVisible(true);
+				pnum.setVisible(true);
+				pnamt.setVisible(true);
+				pphont.setVisible(true);
+				entpat.setVisible(true);
+
 				refresh(DoctorID);
-				
-				
+
 			}
-			
+
 		});
-		
+
 		JButton visits = new JButton("Display Patient Visits.");
 		c2.gridx = 2;
 		c2.gridy = 0;
 		c2.gridwidth = 1;
-		
+
 		panel2.add(visits, c2);
-		
+
 		visits.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		});
-		
+
 		JButton enterv = new JButton("Enter new Visit.");
 		c2.gridx = 3;
 		c2.gridy = 0;
 		c2.gridwidth = 1;
-		
+
 		panel2.add(enterv, c2);
-		
+
 		enterv.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 		});
-		
+
 		frame2.add(panel2);
 		frame2.setContentPane(panel2);
-		
-		//////////////////////New Frame End///////////////////////////////
-		
-		//////////////////////////Button Action Listener START////////////////////////////////
-		
-		enter.addActionListener(new ActionListener(){
+
+		////////////////////// New Frame End///////////////////////////////
+
+		////////////////////////// Button Action Listener START////////////////////////////////
+
+		enter.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				String sql = "SELECT Password FROM DoctorTable WHERE Username = ?";
-				
+
 				try {
 					System.out.println("Enter Button");
 					Connection conn = connect();
@@ -286,105 +309,95 @@ public class DoctorGUI {
 					pstmt.setString(1, usert.getText());
 					ResultSet rs = pstmt.executeQuery();
 					DoctorID = getDoctorID(usert.getText());
-					
-					
-					
-					if(rs.getString("Password").equals(passt.getText())) {
+
+					if (rs.getString("Password").equals(passt.getText())) {
 						frame.dispose();
 						frame2.setVisible(true);
-					}else {
+					} else {
 						passwordcheck.setVisible(true);
 					}
-					
+
 					rs.close();
 					pstmt.close();
 					conn.close();
-				}catch (SQLException e) {
+				} catch (SQLException e) {
 					System.out.println(e.getMessage());
 				}
-				
+
 			}
-			
+
 		});
-		
-		//////////////////////////////Button Action Listener END////////////////////////////
-		
-		
-		
-		
+
+		////////////////////////////// Button Action Listener END////////////////////////////
+
 		frame.add(panel);
 		frame.setVisible(true);
-		
-	
-		
+
 	}
-	
+
 	private int getDoctorID(String usert) {
 		System.out.println("getDoctorID");
 		String sql = "SELECT DoctorID FROM DoctorTable WHERE Username = ?";
-		
+
 		int Doctorid = 0;
-		
+
 		try {
 			Connection conn = connect();
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, usert);
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			Doctorid = rs.getInt("DoctorID");
-			System.out.println("Doctor ID = " + Doctorid);
-			
+
 			rs.close();
 			pstmt.close();
 			conn.close();
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-		
+
 		return Doctorid;
 	}
-	
+
 	private Connection connect() {
 		Connection conn = null;
-		
+
 		try {
 			String url = "jdbc:sqlite:H:\\git\\DoctorDatabase\\DoctorDatabase\\DoctorDatabase.db";
 			conn = DriverManager.getConnection(url);
 			System.out.println("Connection to SQLite has been established.");
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 		return conn;
 	}
-//////Issue	
+
 	public void refresh(int DoctorID) {
 		System.out.println("Refresh method");
 		row = new Object[2];
 		model.setRowCount(0);
 		String sql = "SELECT Name, Phonenumber FROM PatientTable WHERE DoctorID = ?";
-		
-		
+
 		try {
 			Connection conn = connect();
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, DoctorID);
 			ResultSet rs = pstmt.executeQuery();
-			
+
 			while (rs.next()) {
 				row[0] = rs.getString("Name");
 				row[1] = rs.getString("Phonenumber");
-				
+
 				model.addRow(row);
 			}
-			
+
 			rs.close();
 			pstmt.close();
 			conn.close();
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 	}
-
 
 }
